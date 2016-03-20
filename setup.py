2b69c724fcd251b72cabc74c5ae65a01d490beb8
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-03-20 22:11:24 +0000

    MAINT: make it a package
    A very crude first approximation. No idea if e.g. sdist works.
    Making tests a subpackage does not feel right but hey.
    No idea what is the recommendation w.r.t. installing .h files.

--- setup.py
+++ setup.py
@@ -2,24 +2,32 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import os
 import sys
 import numpy
 
+def process_tempita_pyx():
+    # taken from scipy's cythonize.py
+    fromfile = os.path.join("sparr", "sp_map.pyx.in")
+    from Cython import Tempita as tempita
+    from_filename = tempita.Template.from_filename
+    template = from_filename(fromfile, encoding=sys.getdefaultencoding())
+    pyxcontent = template.substitute()
+    assert fromfile.endswith('.pyx.in')
+    pyxfile = fromfile[:-len('.pyx.in')] + '.pyx'
+    with open(pyxfile, "w") as f:
+        f.write(pyxcontent)
 
-fromfile = "sp_map.pyx.in"
-# taken from scipy's cythonize.py
-from Cython import Tempita as tempita
-from_filename = tempita.Template.from_filename
-template = from_filename(fromfile, encoding=sys.getdefaultencoding())
-pyxcontent = template.substitute()
-assert fromfile.endswith('.pyx.in')
-pyxfile = fromfile[:-len('.pyx.in')] + '.pyx'
-with open(pyxfile, "w") as f:
-    f.write(pyxcontent)
+process_tempita_pyx()
 
-ext = Extension("sp_map", ["sp_map.pyx"],
+ext = Extension("sparr._sp_map", [os.path.join("sparr", "sp_map.pyx")],
                 include_dirs = [numpy.get_include()],
                 language="c++",)
                 
-setup(ext_modules=[ext],
+setup(name="sparr",
+      packages=["sparr", "sparr.tests"],
+      version="0.0.1",
+      license="BSD",
+
+      ext_modules=[ext],
       cmdclass = {'build_ext': build_ext})
