e41eee163503ea3dcc40a060c9d1885bfc795697
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2016-04-01 00:09:56 +0100

    MAINT: move index validation to C++
    To minimize the python overhead in get/set-item.
    Also add a hook for freezing the dimensions.

--- sparr/util.h
+++ sparr/util.h
@@ -3,6 +3,7 @@
 #include <Python.h>
 #include "numpy/ndarrayobject.h"
 #include "fixed_cap.h"
+#include "sp_map.h"
 
 
 /*
@@ -35,6 +36,40 @@ _is_tuple_of_integers(PyObject *obj, index_type& idx)
         idx[i] = PyInt_AsLong(x);
     }
     return 1;
+}
+
+
+/*
+ * Check if `idx` is a valid index to the array `*arr`, taking into account negative indices.
+ * For negative indices, modify the `idx` argument in-place.
+ * For invalid indices, raise IndexError.
+ * Depending on the value of `is_shape_fixed`, decide if expanding the array
+ * is allowed or not.
+ *
+ * The dimensionality of `idx` and `arr` is NOT checked and it must be validated
+ * before.
+ */
+template<typename T>
+inline PyObject*
+validate_index(index_type& idx, const map_array_t<T> *arr, int is_shape_fixed)
+{
+    index_type shp = arr->shape();
+    for(size_t j = 0; j < arr->ndim(); ++j){
+        if (idx[j] < 0){
+            idx[j] += shp[j];
+        }
+
+        // check if still out of bounds
+        if ((idx[j] < 0) ||
+            (is_shape_fixed && (idx[j] >= shp[j]))){
+                PyErr_Format(PyExc_IndexError, "index %ld is out of bounds for "
+                            "axis %zu with size %ld", idx[j], j, shp[j]);
+                return NULL;
+        }
+    }
+
+    // return something non-NULL to signal no error
+    Py_RETURN_TRUE;
 }
 
 
